1cf0d0f48e2dff46f5b0b96a11f05854f89d7db3
Louisa Pierre-Marie <emy.pm2016@gmail.com>
2026-09-22 14:13:12 +0200

    Question 3 : classe Habitant avec constructeur

---
+++ tp2/habitant.py
@@ -0,0 +1,18 @@
+class Habitant: 
+    def __init__(self,nom,age,adresse,animaux=None):
+        self.nom = nom
+        self.age = age
+        self.adresse = adresse
+        self.animaux = animaux if animaux is not None else {}
+
+    def affichage_adresse(self):
+        print(f"{self.nom} habite à {self.adresse}")
+
+    def compte_animal(self,animal):
+        return self.animaux.get(animal,0) #on cherche la clé avec get. Si elle existe on l'a récupère. Sinon on renvoie 0
+
+h1 = Habitant("Aldric", 25, "Rue A", {"vaches": 3})
+assert h1.nom == "Aldric"
+assert h1.compte_animal("vaches") == 3
+assert h1.compte_animal("moutons") == 0
+h1.affichage_adresse() # affiche "Aldric habite a Rue A"
